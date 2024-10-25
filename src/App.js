92319a9905a4b9e56cc4d0a1f71dcb5bb0dd99b5
YogitaSahu <yogisahu7896@gmail.com>
2024-10-26 02:13:55 +0530

    Revert "initial commit"
    This reverts commit 980b582ee8804503550aa09c5d13e7ce9c448277.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Main from './Main';
-import Mountain from './Mountain';
-import Birds from './Birds';
-import Beaches from './Beaches';
-import Food from './Food';
+import "./App.css";
+import Contact from "./components/contact";
+
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Main />} />
-          <Route path='/mountain' element={<Mountain />} />
-          <Route path='/birds' element={<Birds />} />
-          <Route path='/beach' element={<Beaches />} />
-          <Route path='/food' element={<Food />} />
-        </Routes>
-      </BrowserRouter>
+      <Contact />
     </div>
   );
 }
