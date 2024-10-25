980b582ee8804503550aa09c5d13e7ce9c448277
YogitaSahu <yogisahu7896@gmail.com>
2024-10-26 02:04:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import "./App.css";
-import Contact from "./components/contact";
-
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Main from './Main';
+import Mountain from './Mountain';
+import Birds from './Birds';
+import Beaches from './Beaches';
+import Food from './Food';
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/mountain' element={<Mountain />} />
+          <Route path='/birds' element={<Birds />} />
+          <Route path='/beach' element={<Beaches />} />
+          <Route path='/food' element={<Food />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
